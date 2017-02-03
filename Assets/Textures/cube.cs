a69b3738ed0c68f40e6e22c64b186a0aa7e3123d
NPKompleet <filip2unine@yahoo.co.uk>
2017-02-03 08:46:25 +0100

    first commit

---
+++ Assets/Textures/cube.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cube : MonoBehaviour {
+	bool collided;
+
+	// Use this for initialization
+	void Start () {
+		collided = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!collided) {
+			this.gameObject.transform.Translate (Vector3.right * Time.deltaTime);
+		}
+	}
+
+	void OnTriggerEnter(Collider other){
+		collided = true;
+		this.gameObject.transform.position = other.gameObject.transform.position;
+	}
+}
---
+++ Assets/Textures/pole.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pole : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "cubey") Debug.Log("A cube along this way came");
+	}
+}
